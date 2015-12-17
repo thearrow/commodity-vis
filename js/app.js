4d1f589762f0e6230e5470215cb8bbf7593a1c4a
Jake Klingler <thearrow3456@gmail.com>
2015-12-16 17:57:04 -0700

    add legend

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ var asks = [];
 var asksByDay;
 var bids = [];
 var bidsByDay;
+var raw = false;
 
 var x = d3.time.scale()
   .range([0, width]);
@@ -33,6 +34,18 @@ var line = d3.svg.line()
   })
   .interpolate('step');
 
+var area = d3.svg.area()
+  .x(function(d) {
+    return x(d.time);
+  })
+  .y0(function() {
+    return 1000;
+  })
+  .y1(function(d) {
+    return y(d.value);
+  })
+  .interpolate('step');
+
 var zoom = d3.behavior.zoom()
     .on('zoom', zoomed);
 
@@ -41,12 +54,34 @@ var toolbar = d3.select('.chart').append('div')
 
 var buttonRawData = toolbar.append('button')
   .text('Raw Data')
-  .on('click', groupNone);
+  .on('click', handleGroupNone);
 
 var buttonDailyAvg = toolbar.append('button')
   .text('Daily Averages')
   .attr('disabled', true)
-  .on('click', groupDay);
+  .on('click', handleGroupDay);
+
+var buttonShowSpread = toolbar.append('button')
+  .text('Show Spread')
+  .attr('disabled', true)
+  .style('margin-left', '50px')
+  .on('click', handleShowSpread);
+
+var buttonHideSpread = toolbar.append('button')
+  .text('Hide Spread')
+  .on('click', handleHideSpread);
+
+var legend = toolbar.append('div')
+  .attr('class', 'legend');
+legend.append('div')
+  .text('- Ask')
+  .attr('class', 'asks');
+legend.append('div')
+  .text('- Bid')
+  .attr('class', 'bids');
+
+var tooltip = toolbar.append('div')
+  .attr('class', 'tooltip');
 
 var svg = d3.select('.chart').append('svg')
   .call(zoom)
@@ -55,13 +90,60 @@ var svg = d3.select('.chart').append('svg')
   .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+svg.append('rect')
+  .attr('class', 'overlay')
+  .attr('width', width)
+  .attr('height', height)
+  .on('mousemove', handleMouseMove);
+
 svg.append('defs').append('clipPath')
   .attr('id', 'clip')
   .append('rect')
     .attr('width', width)
     .attr('height', height);
+svg.select('defs').append('mask')
+  .attr('id', 'area-mask')
+  .append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', 'white');
 
-function groupNone() {
+function findKey(array, key) {
+  return array.filter(function(d) {
+    return d.key.indexOf(key) > -1;
+  });
+}
+
+function handleMouseMove() {
+  var date = x.invert(d3.mouse(this)[0]);
+  var dateText = dayFormatter(date);
+  var askText = findKey(asksByDay, dateText);
+  var bidText = findKey(bidsByDay, dateText);
+
+  askText = parseNumberFromArrayOrEmpty(askText);
+  bidText = parseNumberFromArrayOrEmpty(bidText);
+
+  tooltip
+    .text(dateText +
+      '  Ask: ' + askText +
+      '  Bid: ' + bidText);
+  d3.select('.tooltip-line')
+    .attr('x1', d3.mouse(this)[0])
+    .attr('x2', d3.mouse(this)[0]);
+}
+
+function parseNumberFromArrayOrEmpty(input) {
+  var output;
+  if (input.length > 0) {
+    output = input[0].value.toFixed(1);
+  } else {
+    output = '';
+  }
+  return output;
+}
+
+function handleGroupNone() {
+  raw = true;
   buttonDailyAvg.attr('disabled', null);
   buttonRawData.attr('disabled', true);
 
@@ -71,9 +153,11 @@ function groupNone() {
   svg.select('.line.bids')
     .datum(bids)
     .attr('d', line);
+  renderAreas(raw);
 }
 
-function groupDay() {
+function handleGroupDay() {
+  raw = false;
   buttonDailyAvg.attr('disabled', true);
   buttonRawData.attr('disabled', null);
 
@@ -83,11 +167,45 @@ function groupDay() {
   svg.select('.line.bids')
     .datum(bidsByDay)
     .attr('d', line);
+  renderAreas(raw);
+}
+
+function handleShowSpread() {
+  buttonShowSpread.attr('disabled', true);
+  buttonHideSpread.attr('disabled', null);
+  d3.select('.area')
+    .style('display', '');
+}
+
+function handleHideSpread() {
+  buttonShowSpread.attr('disabled', null);
+  buttonHideSpread.attr('disabled', true);
+  d3.select('.area')
+    .style('display', 'none');
+}
+
+function renderAreas(raw) {
+  if (raw) {
+    svg.selectAll('path.area.asks').attr('d', function() {
+      return area(asks);
+    });
+    svg.selectAll('.bids-mask').attr('d', function() {
+      return area(bids);
+    });
+  } else {
+    svg.selectAll('path.area.asks').attr('d', function() {
+      return area(asksByDay);
+    });
+    svg.selectAll('.bids-mask').attr('d', function() {
+      return area(bidsByDay);
+    });
+  }
 }
 
 function zoomed() {
   svg.select('.x.axis').call(xAxis);
   svg.selectAll('path.line').attr('d', line);
+  renderAreas(raw);
 }
 
 function sortByTime(a, b) {
@@ -169,4 +287,24 @@ d3.csv('../data.csv', function(error, data) {
     .attr('class', 'line bids')
     .attr('d', line)
     .attr('clip-path', 'url(#clip)');
+
+  svg.select('#area-mask').append('path')
+    .attr('class', 'bids-mask')
+    .attr('d', function() {
+      return area(bidsByDay);
+    });
+
+  svg.append('path')
+    .attr('class', 'area asks')
+    .attr('mask', 'url(#area-mask)')
+    .attr('d', function() {
+      return area(asksByDay);
+    });
+
+  svg.append('line')
+    .attr('class', 'tooltip-line')
+    .attr('x1', 0)
+    .attr('x2', 0)
+    .attr('y1', 0)
+    .attr('y2', height);
 });
